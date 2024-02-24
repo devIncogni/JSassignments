5b10280143d33a91a33fd973493e882f95d545fd
devIncogni <arinkumar10@outlook.com>
2024-02-24 23:48:44 +0530

    completed UI for Stone Paper Scissor

--- script.js
+++ script.js
@@ -5,28 +5,69 @@ function getComputerChoice() {
   return arr[Math.floor(num)];
 }
 
+let playerScore = 0;
+let computerScore = 0;
+let ties = 0;
+
 function playRound(playerChoice = "", computerChoice = getComputerChoice()) {
   if (playerChoice.toLowerCase() === computerChoice) {
-    return "Tie";
+    ties++;
+    return ("Tie");
   } else if (
     (playerChoice.toLowerCase() === "stone" && computerChoice === "scissor") ||
     (playerChoice.toLowerCase() === "paper" && computerChoice === "stone") ||
-    (playerChoice.toLowerCase() === "paper" && computerChoice === "stone")
+    (playerChoice.toLowerCase() === "scissor" && computerChoice === "paper")
   ) {
-    return "You Win! " + playerChoice.toLowerCase() + " beats " + computerChoice;
+    playerScore++;
+    return ("You Win! " + playerChoice.toUpperCase() + " beats " + computerChoice);
   } else {
-    return "You Loose! " + computerChoice + " beats " + playerChoice.toLowerCase();
+    computerScore++;
+    return ("You Loose! " + computerChoice + " beats " + playerChoice.toUpperCase());
   }
 }
 
-function playGame() {
-  for (let index = 0; index < 5; index++) {
-    let playerChoice = prompt("Enter stone paper or scissior");
-    console.log(playRound(playerChoice, getComputerChoice()));
-  }
+// function playGame() {
+//   for (let index = 0; index < 5; index++) {
+//     let playerChoice = prompt("Enter stone paper or scissior");
+//     console.log(playRound(playerChoice, getComputerChoice()));
+//   }
+// }
+
+// Play game DOM
+
+function returnScoreString() {
+  return ("Computer Score: "+computerScore+" Player Score: "+playerScore+" Ties: "+ties);  
 }
 
-playGame()
+/** 
+ * @param {Event & {target: Node}} event
+ */
+
+function playerChooses(event) {
+  roundResult.textContent = (playRound(event.target.textContent));
+  score.textContent = returnScoreString();  
+  // console.log();
+}
+
+/**
+ * @param {Node} aButton
+ */
+
+function bttn(aButton) {
+  aButton.addEventListener("click", playerChooses);
+}
+
+const roundResult = document.querySelector(".roundResult");
+const score = document.querySelector(".score");
+const btn = document.querySelectorAll('.btnHolder > button');
+btn.forEach(bttn)
+
+
+// roundResult.textContent
+
+// btn.addEventListener("click", playerChooses)
+
+// playGame()
 // const playerSelection = "stone";
 // const computerSelection = getComputerChoice();
 // console.log(playRound(playerSelection, computerSelection));
